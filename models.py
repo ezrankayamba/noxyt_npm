ace12d620116933eaf448f169dd29d36fe739118
ezrankayamba <ezrankayamba@gmail.com>
2021-02-09 20:58:39 +0300

    more error info

--- models.py
+++ models.py
@@ -68,7 +68,7 @@ class Message:
             cursor.execute('SELECT * FROM npm_messages where status=0 and email=%s', (email))
             res = [Message(row) for row in cursor.fetchall()]
         except Exception as e:
-            print("Error recording message2", e)
+            print("Error recording message2", e, email)
         finally:
             if cursor:
                 cursor.close()
